ea0c1221663d3f7b39f192213a21c0f29681572c
almorish123321-creator <almorish123321@gmail.com>
2026-05-27 00:39:33 +0300

    Change SECRET_KEY to a generic placeholder
    Updated the SECRET_KEY configuration for security.

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ from src.routes.user import user_bp
 from src.routes.medical_leaves import medical_leaves_bp
 
 app = Flask(__name__, static_folder=os.path.join(os.path.dirname(__file__), 'static'))
-app.config['SECRET_KEY'] = 'asdf#FGSgvasgf$5$WGT'
+app.config['SECRET_KEY'] = 'SECRET_KEY'
 
 # تفعيل CORS
 CORS(app)
