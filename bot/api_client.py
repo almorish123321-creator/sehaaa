97e3b758654e19b5f52b1a70f7aee7977a9e83e4
almorish123321-creator <almorish123321@gmail.com>
2026-05-27 00:59:05 +0300

    Change import from config to config_env

--- bot/api_client.py
+++ bot/api_client.py
@@ -9,7 +9,7 @@ import requests
 import json
 import logging
 from datetime import datetime
-from config import API_FULL_URL
+from config_env import API_FULL_URL
 
 # إعداد التسجيل
 logger = logging.getLogger(__name__)
